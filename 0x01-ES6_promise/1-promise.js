b8deb86658881161c1a8ff16aed86483cbd4a0cc
HazelDaniel <hazeldaniel856@gmail.com>
2024-03-06 16:37:11 +0100

    task-1: re-submission

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 function getFullResponseFromAPI(success) {
   return new Promise((res, rej) => {
     if (success) {
-      res({ status: 200, body: 'success' });
+      res({ status: 200, body: 'Success' });
     } else {
       rej(new Error('The fake API is not working currently'));
     }
