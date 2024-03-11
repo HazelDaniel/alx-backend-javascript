6377409f78021e26ec6fd03e1bd1d8184a547d0c
HazelDaniel <hazeldaniel856@gmail.com>
2024-03-11 23:58:44 +0100

    task-5: re-submission

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw new Error('Position out of range');
+    throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
