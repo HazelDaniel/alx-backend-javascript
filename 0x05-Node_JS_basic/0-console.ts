c200c56ad64b16278467378425e6edd2c7ef6184
HazelDaniel <hazeldaniel856@gmail.com>
2024-05-02 00:30:37 +0100

    cleaned up the ts output

--- 0x05-Node_JS_basic/0-console.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const displayMessage: () => void = () => {
-  console.log("Hello NodeJS!");
-};
-
-module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.ts
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/node
-console.log("Welcome to Holberton School, what is your name?");
