77a310d3c7538203d52185acf742dfad8aeaa29e
HazelDaniel <hazeldaniel856@gmail.com>
2024-05-03 22:17:05 +0100

    task-10: submission

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 const express = require('express');
 
 const app = express();
@@ -9,14 +10,14 @@ app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
 app.get('/cart/:id(\\d+)', (req, res) => {
   const id = req.params.id;
 
   res.send(`Payment methods for cart ${id}`);
-});
-
-app.get('/available_payments', (_req, res) => {
-  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
 app.post('/login', (req, res) => {
